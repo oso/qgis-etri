874fabd88fed8261aaf231191c52661b1f093c1d
Olivier Sobrie <olivier.sobrie@gmail.com>
2010-07-29 22:18:27 +0200

    [TEST] Add old test

---
+++ tests/qgis_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+from qgis.core import *
+import sys
+
+def load_layer(path, name):
+	layerPath = path
+	layerName = name
+	layerProvider = "ogr"
+	layer = QgsVectorLayer(layerPath, layerName, layerProvider)
+	if not layer.isValid():
+		raise NameError,"Layer failed to load!"
+
+	return layer
+
+def get_field_list(provider):
+	fields = provider.fields()
+	a = []
+	for (i, field) in fields.iteritems():
+		print i,":", field.name().trimmed()
+		str = '%s' % field.name().trimmed()
+		#b = 'coucou'
+		a.append(i)
+	
+	print a
+
+	return fields
+
+def main(argv):
+	QgsApplication.setPrefixPath("/usr", True)
+	QgsApplication.initQgis()
+
+	layer = load_layer("data/criterions.shp", "criterions")
+
+	provider = layer.dataProvider()
+	fields = get_field_list(provider)
+
+	QgsApplication.exitQgis()
+
+
+if __name__ == "__main__":
+	main(sys.argv)
