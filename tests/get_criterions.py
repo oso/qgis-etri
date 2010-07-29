f4a868d75cc9c30be59660b04d55acf6cea94a0d
Olivier Sobrie <olivier.sobrie@gmail.com>
2010-07-29 22:17:03 +0200

    [TEST] Function to get the actions in a list

--- tests/get_criterions.py
+++ tests/get_criterions.py
@@ -1,6 +1,49 @@
 from qgis.core import *
 import sys
 
+class qgis_criterions_layer:
+
+	def __init__(self, layer):
+		self.layer = layer
+		self.provider = self.layer.dataProvider()
+		self.criterions = self.provider.attributeIndexes()
+
+	def get_fields(self):
+		return self.provider.fields()
+
+	def get_fieldnames(self):
+		fields = self.provider.fields()
+		names = []
+		for (i, field) in fields.iteritems():
+			str = '"%s"' % field.name().trimmed()
+			names.append(str)
+		return names
+
+	def get_criterion_names(self):
+		fields = self.provider.fields()
+		names = []
+		for (i, field) in fields.iteritems():
+			if i in self.criterions:
+				name = '"%s"' % field.name().trimmed()
+				names.append(name)
+		return names
+
+	def get_actions(self):
+		feat = QgsFeature()
+		self.provider.select(self.criterions, QgsRectangle(), False)
+	
+		actions = []
+		while self.provider.nextFeature(feat):
+			attrs = feat.attributeMap()
+			action = []
+			for (k,attr) in attrs.iteritems():
+				val = attr.toDouble()
+				action.append(val[0])
+	
+			actions.append(action)
+	
+		return actions
+
 def main(argv):
 	QgsApplication.setPrefixPath("/usr", True)
 	QgsApplication.initQgis()
@@ -10,33 +53,15 @@ def main(argv):
 	layerProvider = "ogr"
 
 	layer = QgsVectorLayer(layerPath, layerName, layerProvider)
-
 	if not layer.isValid():
 		print "Layer failed to load!"
 
-	provider = layer.dataProvider()
-	feat = QgsFeature()
-	allAttrs = provider.attributeIndexes()
-	print "attributes", allAttrs
-	allAttrs = [1, 2, 3]
-	print "attributes", allAttrs
-	provider.select(allAttrs, QgsRectangle(), False)
-
-	actions = []
-	while provider.nextFeature(feat):
-#		print "Feature ID %d: \n" % feat.id() ,
-		attrs = feat.attributeMap()
-		action = []
-		for (k,attr) in attrs.iteritems():
-			val = attr.toDouble()
-			action.append(val[0])
-
-		print action
-		actions.append(action)
-
-#	print actions
+	clayer = qgis_criterions_layer(layer)
+	print "fields\n", clayer.get_fieldnames()
+	print "criterions\n", clayer.get_criterion_names()
+	print "actions\n", clayer.get_actions()
 
 	QgsApplication.exitQgis()
-
+	
 if __name__ == "__main__":
 	main(sys.argv)
