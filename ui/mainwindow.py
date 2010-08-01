d89a5e9bfaa4052aed8ff9d512afec3bc1795eb0
Olivier Sobrie <olivier.sobrie@gmail.com>
2010-08-01 22:10:59 +0200

    [APP] Indentation update to 4 spaces instead of tabs

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -10,26 +10,26 @@ criterions = [ "Densite_POP", "Tx chomage", "PIB" ]
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
-	def __init__(self, parent = None):
-		QtGui.QMainWindow.__init__(self, parent)
-		self.setupUi(self)
-		self.table_crit.setColumnWidth(0, 200)
-		self.table_crit.setColumnWidth(1, 50)
-		self.table_prof.setColumnWidth(0, 50)
-		self.table_prof.setColumnWidth(1, 50)
-		self.table_prof.setColumnWidth(2, 50)
-		self.set_data()
+    def __init__(self, parent = None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+        self.table_crit.setColumnWidth(0, 200)
+        self.table_crit.setColumnWidth(1, 50)
+        self.table_prof.setColumnWidth(0, 50)
+        self.table_prof.setColumnWidth(1, 50)
+        self.table_prof.setColumnWidth(2, 50)
+        self.set_data()
 
-	def set_data(self):
-		for crit in criterions:
-			nrow = self.table_crit.rowCount()
-
-			self.table_crit.insertRow(nrow)
-			item = QtGui.QTableWidgetItem()
-			item.setFlags(QtCore.Qt.ItemIsTristate)
-			self.table_crit.setItem(nrow, 0, item)
-
-			checkbox = QtGui.QCheckBox(self)
-			checkbox.setCheckState(QtCore.Qt.Checked)
-			checkbox.setText(QtGui.QApplication.translate("MainWindow", crit, None, QtGui.QApplication.UnicodeUTF8))
-			self.table_crit.setCellWidget(nrow, 0, checkbox)
+    def set_data(self):
+        for crit in criterions:
+            nrow = self.table_crit.rowCount()
+            
+            self.table_crit.insertRow(nrow)
+            item = QtGui.QTableWidgetItem()
+            item.setFlags(QtCore.Qt.ItemIsTristate)
+            self.table_crit.setItem(nrow, 0, item)
+            
+            checkbox = QtGui.QCheckBox(self)
+            checkbox.setCheckState(QtCore.Qt.Checked)
+            checkbox.setText(QtGui.QApplication.translate("MainWindow", crit, None, QtGui.QApplication.UnicodeUTF8))
+            self.table_crit.setCellWidget(nrow, 0, checkbox)
