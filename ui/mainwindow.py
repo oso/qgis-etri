61e8c4f8fe06757af08ea6def5f74b073738d046
Olivier Sobrie <olivier.sobrie@gmail.com>
2010-08-01 22:06:08 +0200

    [APP] Update application mainwindow

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -6,81 +6,30 @@ from qgis.gui import *
 
 from Ui_mainwindow import Ui_MainWindow
 
+criterions = [ "Densite_POP", "Tx chomage", "PIB" ]
+
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
 	def __init__(self, parent = None):
 		QtGui.QMainWindow.__init__(self, parent)
 		self.setupUi(self)
+		self.table_crit.setColumnWidth(0, 200)
+		self.table_crit.setColumnWidth(1, 50)
+		self.table_prof.setColumnWidth(0, 50)
+		self.table_prof.setColumnWidth(1, 50)
+		self.table_prof.setColumnWidth(2, 50)
+		self.set_data()
 
-		self.canvas = QgsMapCanvas()
-		self.canvas.setCanvasColor(QtGui.QColor(255,255,255))
-		self.canvas.enableAntiAliasing(True)
-		self.canvas.show()
+	def set_data(self):
+		for crit in criterions:
+			nrow = self.table_crit.rowCount()
 
-		self.layout = QtGui.QVBoxLayout(self.map_frame)
-		self.layout.addWidget(self.canvas)
+			self.table_crit.insertRow(nrow)
+			item = QtGui.QTableWidgetItem()
+			item.setFlags(QtCore.Qt.ItemIsTristate)
+			self.table_crit.setItem(nrow, 0, item)
 
-		# create the actions behaviours
-		self.connect(self.mpActionAddLayer, QtCore.SIGNAL("triggered()"), self.addLayer)
-#		self.connect(self.mpActionZoomIn, QtCore.SIGNAL("triggered()"), self.zoomIn)
-#		self.connect(self.mpActionZoomOut, QtCore.SIGNAL("triggered()"), self.zoomOut)
-#		self.connect(self.mpActionPan, QtCore.SIGNAL("triggered()"), self.pan)
-
-		# create a little toolbar
-		self.toolbar = self.addToolBar("File");
-		self.toolbar.addAction(self.mpActionAddLayer);
-#		self.toolbar.addAction(self.mpActionZoomIn);
-#		self.toolbar.addAction(self.mpActionZoomOut);
-#		self.toolbar.addAction(self.mpActionPan);
-
-#		# create the map tools
-#		self.toolPan = QgsMapToolPan(self.canvas)
-#		self.toolPan.setAction(self.mpActionPan)
-#		self.toolZoomIn = QgsMapToolZoom(self.canvas, False)
-#		self.toolZoomIn.setAction(self.mpActionZoomIn)
-#		self.toolZoomOut = QgsMapToolZoom(self.canvas, True)
-#		self.toolZoomOut.setAction(self.mpActionZoomOut)
-
-	def zoomIn(self):
-		self.canvas.setMapTool(self.toolZoomIn)
-
-	def zoomOut(self):
-		self.canvas.setMapTool(self.toolZoomOut)
-
-	def pan(self):
-		self.canvas.setMapTool(self.toolPan)
-
-	def addLayer(self):
-		"""add a (hardcoded) layer and zoom to its extent"""
-
-		layerPath = "/home/oso/qgis_france/DEPARTEMENT.SHP"
-		layerName = "DEPARTEMENT"
-		layerProvider = "ogr"
-		layer = QgsVectorLayer(layerPath, layerName, layerProvider)
-
-		if not layer.isValid():
-			return
-
-		QgsMapLayerRegistry.instance().addMapLayer(layer);
-
-		self.canvas.setExtent(layer.extent())
-
-		cl = QgsMapCanvasLayer(layer)
-		layers = [cl]
-		self.canvas.setLayerSet(layers)
-
-#		layerPath = "/home/oso/qgis_france/DEPARTEMENT.SHP"
-#		layerName = "DEPARTEMENT"
-#		layerProvider = "ogr"
-#		layer = QgsVectorLayer(layerPath, layerName, layerProvider)
-#
-#		if not layer.isValid():
-#			return
-#
-#		QgsMapLayerRegistry.instance().addMapLayer(layer);
-#
-#		self.canvas.setExtent(layer.extent())
-#
-#		cl = QgsMapCanvasLayer(layer)
-#		layers = [cl]
-#		self.canvas.setLayerSet(layers)
+			checkbox = QtGui.QCheckBox(self)
+			checkbox.setCheckState(QtCore.Qt.Checked)
+			checkbox.setText(QtGui.QApplication.translate("MainWindow", crit, None, QtGui.QApplication.UnicodeUTF8))
+			self.table_crit.setCellWidget(nrow, 0, checkbox)
