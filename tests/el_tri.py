90fc23abad0affbae4440b18d9da94355432ec89
Olivier Sobrie <olivier@sobrie.be>
2010-08-04 21:50:46 +0200

    [TEST] Update electre tri class (use dictionnary for profiles)

--- tests/el_tri.py
+++ tests/el_tri.py
@@ -26,126 +26,127 @@ q = [15,   80, 1, 0.5, 1]
 p = [40,  350, 3, 3.5, 5]
 v = [100, 850, 5, 4.5, 8]
 
+prof1 = { 'refs': b1, 'q': q, 'p': p, 'v': v }
+prof2 = { 'refs': b2, 'q': q, 'p': p, 'v': v }
+profiles = [ prof1, prof2 ]
+
 # Lambda
 lbda = 0.75
 
 class electre_tri:
 
-	def __init__(self, a, b, q, p, v, lbda):
-		self.a = a
-		self.b = b
-		self.q = q
-		self.p = p
-		self.v = v
-		self.lbda = lbda
+    def __init__(self, actions, profiles, lbda):
+        self.a = actions
+        self.profiles = profiles
+        self.lbda = lbda
 
-	def v_substract(self, x, y):
-		return [x - y for x, y in zip(x, y)]
+    def v_substract(self, x, y):
+        return [x - y for x, y in zip(x, y)]
 
-	def v_multiply(self, x, y):
-		return [x * y for x, y in zip(x, y)]
+    def v_multiply(self, x, y):
+        return [x * y for x, y in zip(x, y)]
 
-	def partial_concordance(self, x, y):
-		# compute g_j(b) - g_j(a)
-		diff = self.v_substract(y, x)
+    def partial_concordance(self, x, y, q, p):
+        # compute g_j(b) - g_j(a)
+        diff = self.v_substract(y, x)
 
-		# compute c_j(a, b)
-		c = []
-		for i in range(len(diff)):
-			if diff[i] > self.p[i]:
-				c.append(0)
-			elif diff[i] <= self.q[i]:
-				c.append(1)
-			else:
-				num = float(self.p[i]-diff[i])
-				den = float(self.p[i]-self.q[i])
-				c.append(num/den)
-	
-		return c
+        # compute c_j(a, b)
+        c = []
+        for i in range(len(diff)):
+            if diff[i] > p[i]:
+                c.append(0)
+            elif diff[i] <= q[i]:
+                c.append(1)
+            else:
+                num = float(p[i]-diff[i])
+                den = float(p[i]-q[i])
+                c.append(num/den)
 
-	def concordance(self, A, B):
-		cj = self.partial_concordance(A, B)
-		pjcj = float(sum(self.v_multiply(w, cj)))
-		wsum = float(sum(w))
-		return pjcj/wsum
+        return c
 
-	def partial_discordance(self, x, y):
-		# compute g_j(b) - g_j(a)
-		diff = self.v_substract(y, x)
-	
-		# compute d_j(a,b)
-		d = []
-		for i in range(len(diff)):
-			if diff[i] > self.v[i]:
-				d.append(1)
-			elif diff[i] <= self.p[i]:
-				d.append(0)
-			else:
-				num = float(self.v[i]-diff[i])
-				den = float(self.v[i]-self.p[i])
-				d.append(1-num/den)
-	
-		return d
-	
-	def credibility(self, x, y):
-		dj = self.partial_discordance(x, y)
-		C = self.concordance(x, y)
-	
-		sigma = C
-		for disc in dj:
-			if disc > C:
-				num = float(1-disc)
-				den = float(1-C)
-				sigma = sigma*num/den
-	
-		return sigma
-	
-	def outrank(self, action, profil, lbda):
-		s_ab = self.credibility(action, profil)
-		s_ba = self.credibility(profil, action)
-	
-		if s_ab >= lbda:
-			if s_ba >= lbda:
-				return "I"
-			else:
-				return "S"
-		else:
-			if s_ba >= lbda:
-				return "-"
-			else:
-				return "R"
-	
-	def pessimist(self):
-		profils = self.b
-		profils.reverse()
-		nprofils = len(b)+1
-		affectations = []
-		for action in self.a:
-			category = nprofils 
-			for profil in profils:
-				outr = self.outrank(action, profil, self.lbda)
-				if outr != "S" and outr != "I":
-					category -= 1
-	
-			affectations.append(category)
-	
-		return affectations
-	
-	def optimist(self):
-		profils = self.b
-		affectations = []
-		for action in self.a:
-			category = 1
-			for profil in profils:
-				outr = self.outrank(action, profil, self.lbda)
-				if outr != "-":
-					category += 1
-	
-			affectations.append(category)
-	
-		return affectations
+    def concordance(self, x, y, q, p):
+        cj = self.partial_concordance(x, y, q, p)
+        pjcj = float(sum(self.v_multiply(w, cj)))
+        wsum = float(sum(w))
+        return pjcj/wsum
 
-etri = electre_tri(a, b, q, p, v, lbda)
+    def partial_discordance(self, x, y, p, v):
+        # compute g_j(b) - g_j(a)
+        diff = self.v_substract(y, x)
+
+        # compute d_j(a,b)
+        d = []
+        for i in range(len(diff)):
+            if diff[i] > v[i]:
+                d.append(1)
+            elif diff[i] <= p[i]:
+                d.append(0)
+            else:
+                num = float(v[i]-diff[i])
+                den = float(v[i]-p[i])
+                d.append(1-num/den)
+
+        return d
+
+    def credibility(self, x, y, q, p, v):
+        dj = self.partial_discordance(x, y, p, v)
+        C = self.concordance(x, y, q, p)
+
+        sigma = C
+        for disc in dj:
+            if disc > C:
+                num = float(1-disc)
+                den = float(1-C)
+                sigma = sigma*num/den
+
+        return sigma
+    
+    def outrank(self, action, profil, lbda):
+        s_ab = self.credibility(action, profil['refs'], profil['q'], profil['p'], profil['v'])
+        s_ba = self.credibility(profil['refs'], action, profil['q'], profil['p'], profil['v'])
+
+        if s_ab >= lbda:
+            if s_ba >= lbda:
+                return "I"
+            else:
+                return "S"
+        else:
+            if s_ba >= lbda:
+                return "-"
+            else:
+                return "R"
+    
+    def pessimist(self):
+        profils = self.profiles
+        profils.reverse()
+        nprofils = len(b)+1
+        affectations = []
+        for action in self.a:
+            category = nprofils 
+            for profil in profils:
+                outr = self.outrank(action, profil, self.lbda)
+                if outr != "S" and outr != "I":
+                    category -= 1
+    
+            affectations.append(category)
+    
+        return affectations
+    
+    def optimist(self):
+        profils = self.profiles
+        affectations = []
+        for action in self.a:
+            category = 1
+            for profil in profils:
+                outr = self.outrank(action, profil, self.lbda)
+                if outr != "-":
+                    category += 1
+    
+            affectations.append(category)
+    
+        return affectations
+
+etri = electre_tri(a, profiles, lbda)
 print "ELECTRE TRI - Pessimist"
 print etri.pessimist()
 print "ELECTRE TRI - Optimist"
