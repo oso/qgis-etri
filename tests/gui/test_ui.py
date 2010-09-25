62c0df121cfe795f8ab2f875543e589c8d70b792
Olivier Sobrie <olivier@sobrie.be>
2010-09-25 14:43:11 +0200

    [TESTS] Remove useless tests

--- tests/gui/test_ui.py
+++
@@ -1,9 +0,0 @@
-from PyQt4 import QtCore, QtGui
-from ui.mainwindow import MainWindow
-
-if __name__ == "__main__":
-	import sys
-	app = QtGui.QApplication(sys.argv)
-	ui = MainWindow()
-	ui.show()
-	sys.exit(app.exec_())
