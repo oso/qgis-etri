oso/qgis-etri

commit b79030c6850d224afc168b459c93eee0857afee7

[XMCDA] Add the possibility to save and load parameters as XMCDA file

etrimain.py
@@ -9,6 +9,7 @@ from xml.etree import ElementTree as ET
 import time
 import threading
 import xmcda
+import ui_utils
 import PyXMCDA
 from Ui_pwdialog import *
 
@@ -80,6 +81,8 @@ class EtriMainWindow(QtGui.QMainWindow, Ui_EtriMainWindow):
         self.Bdel_profile.setEnabled(True)
         self.Bgenerate.setEnabled(True)
         self.Bchooserefs.setEnabled(True)
+        self.Bloadxmcda.setEnabled(True)
+        self.Bsavexmcda.setEnabled(True)
 
     def crit_layer_load(self, layer):
         self.criteria = layer_get_criteria(layer)
@@ -636,6 +639,7 @@ class EtriMainWindow(QtGui.QMainWindow, Ui_EtriMainWindow):
         for i in self.criteria_activated:
             criterion_names["%d" % i] = self.criteria[i]['name'] 
         
+        weights = [(weight*100) for weight in weights]
         inference_dialog.set_weights(criterion_names, weights)
         inference_dialog.set_profiles(criterion_names, profiles)
 
@@ -663,16 +667,17 @@ class EtriMainWindow(QtGui.QMainWindow, Ui_EtriMainWindow):
 
     def set_weights(self, weights):
         for crit_id, val in weights.iteritems():
-            value = "%.2f" % (val*100)
+            value = "%.2f" % val
             id = int(crit_id)
             self.table_crit.item(id,2).setText(value)
 
     def set_profiles(self, profiles):
+        nprofiles = len(profiles)
         self.clear_rows(self.table_prof)
         self.clear_rows(self.table_indiff)
         self.clear_rows(self.table_pref)
         self.clear_rows(self.table_veto)
-        for i in range(len(profiles)):
+        for i in range(nprofiles):
             profile_name = "b%d" % (i+1)
             values = []
             for j in range(len(self.criteria)):
@@ -711,6 +716,52 @@ class EtriMainWindow(QtGui.QMainWindow, Ui_EtriMainWindow):
 
         if self.inf_solution and self.inf_canceled == 0:
             self.display_inference_results(self.inf_solution)
+
+    def on_Bloadxmcda_pressed(self):
+        (file, encoding) = ui_utils.xmcda_load_dialog(self)
+        if file is None:
+            return
+
+        xmcda_file = PyXMCDA.parseValidate(file) 
+        criteria = PyXMCDA.getCriteriaID(xmcda_file)
+        # FIXME: use ID of criterion
+        for i, crit in enumerate(self.criteria):
+            item = self.table_crit.cellWidget(i, 0)
+            if str(i) not in criteria:
+                item.setChecked(False)
+            else:
+                item.setChecked(True)
+
+        profile_names = PyXMCDA.getAlternativesID(xmcda_file)
+        profiles = PyXMCDA.getPerformanceTable(xmcda_file, profile_names, criteria) 
+        weights = PyXMCDA.getCriterionValue(xmcda_file, criteria)
+        compat_alts = PyXMCDA.getAlternativesID(xmcda_file)
+        lbda = xmcda.get_lambda(xmcda_file) 
+
+        self.set_weights(weights)
+        self.set_profiles(profiles)
+        self.set_lambda(lbda)
+
+    def on_Bsavexmcda_pressed(self):
+        weights = self.get_criteria_weights()
+        criteria = weights.keys()
+        nprofiles = self.table_prof.rowCount()
+        profile_names = [ "b%d" % (i+1) for i in range(nprofiles)]
+        profiles = self.get_profiles()
+        cutlevel = self.spinbox_cutlevel.value()
+
+        xmcda_criteria = xmcda.format_criteria(criteria)
+        xmcda_profiles = xmcda.format_pt_reference_alternatives(profiles, profile_names, criteria)
+        xmcda_profile_names = xmcda.format_alternatives(profile_names)
+        xmcda_weights = xmcda.format_criteria_weights(weights)
+        xmcda_cutlevel = xmcda.format_lambda(cutlevel)
+
+        (file, encoding) = ui_utils.xmcda_save_dialog(self)
+        if file is None:
+            return
+
+        # FIXME: encoding
+        xmcda.save_file(file, xmcda_criteria+xmcda_profile_names+xmcda_profiles+xmcda_weights+xmcda_cutlevel) 
 
 class inference_task(threading.Thread):
 

Ui_etrimain.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'etrimain.ui'
 #
-# Created: Thu Nov 25 20:29:27 2010
+# Created: Thu Nov 25 20:59:28 2010
 #      by: PyQt4 UI code generator 4.7.7
 #
 # WARNING! All changes made in this file will be lost!
@@ -154,6 +154,19 @@ class Ui_EtriMainWindow(object):
         self.right_layout.setObjectName(_fromUtf8("right_layout"))
         spacerItem = QtGui.QSpacerItem(20, 20, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
         self.right_layout.addItem(spacerItem)
+        self.group_options = QtGui.QGroupBox(self.centralwidget)
+        self.group_options.setObjectName(_fromUtf8("group_options"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.group_options)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.Bloadxmcda = QtGui.QPushButton(self.group_options)
+        self.Bloadxmcda.setEnabled(False)
+        self.Bloadxmcda.setObjectName(_fromUtf8("Bloadxmcda"))
+        self.verticalLayout_2.addWidget(self.Bloadxmcda)
+        self.Bsavexmcda = QtGui.QPushButton(self.group_options)
+        self.Bsavexmcda.setEnabled(False)
+        self.Bsavexmcda.setObjectName(_fromUtf8("Bsavexmcda"))
+        self.verticalLayout_2.addWidget(self.Bsavexmcda)
+        self.right_layout.addWidget(self.group_options)
         self.group_input = QtGui.QGroupBox(self.centralwidget)
         self.group_input.setObjectName(_fromUtf8("group_input"))
         self.gridLayout_9 = QtGui.QGridLayout(self.group_input)
@@ -281,6 +294,9 @@ class Ui_EtriMainWindow(object):
         self.label_inference.setText(QtGui.QApplication.translate("EtriMainWindow", "Inference:", None, QtGui.QApplication.UnicodeUTF8))
         self.combo_inference.setItemText(0, QtGui.QApplication.translate("EtriMainWindow", "Global inference", None, QtGui.QApplication.UnicodeUTF8))
         self.Tab_params.setTabText(self.Tab_params.indexOf(self.tab_inference), QtGui.QApplication.translate("EtriMainWindow", "Inference", None, QtGui.QApplication.UnicodeUTF8))
+        self.group_options.setTitle(QtGui.QApplication.translate("EtriMainWindow", "XMCDA", None, QtGui.QApplication.UnicodeUTF8))
+        self.Bloadxmcda.setText(QtGui.QApplication.translate("EtriMainWindow", "Load parameters", None, QtGui.QApplication.UnicodeUTF8))
+        self.Bsavexmcda.setText(QtGui.QApplication.translate("EtriMainWindow", "Save parameters", None, QtGui.QApplication.UnicodeUTF8))
         self.group_input.setTitle(QtGui.QApplication.translate("EtriMainWindow", "Input Layer", None, QtGui.QApplication.UnicodeUTF8))
         self.Bloadlayer.setText(QtGui.QApplication.translate("EtriMainWindow", "Load", None, QtGui.QApplication.UnicodeUTF8))
         self.group_profiles.setTitle(QtGui.QApplication.translate("EtriMainWindow", "Categories", None, QtGui.QApplication.UnicodeUTF8))

etrimain.ui
@@ -269,6 +269,35 @@
        </spacer>
       </item>
       <item>
+       <widget class="QGroupBox" name="group_options">
+        <property name="title">
+         <string>XMCDA</string>
+        </property>
+        <layout class="QVBoxLayout" name="verticalLayout_2">
+         <item>
+          <widget class="QPushButton" name="Bloadxmcda">
+           <property name="enabled">
+            <bool>false</bool>
+           </property>
+           <property name="text">
+            <string>Load parameters</string>
+           </property>
+          </widget>
+         </item>
+         <item>
+          <widget class="QPushButton" name="Bsavexmcda">
+           <property name="enabled">
+            <bool>false</bool>
+           </property>
+           <property name="text">
+            <string>Save parameters</string>
+           </property>
+          </widget>
+         </item>
+        </layout>
+       </widget>
+      </item>
+      <item>
        <widget class="QGroupBox" name="group_input">
         <property name="title">
          <string>Input Layer</string>

ui_utils.py
@@ -1,0 +1,36 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+from qgis.gui import *
+
+def xmcda_save_dialog(parent):
+    settings = QSettings() 
+    dirName = settings.value("/UI/lastXMCDAfileDir").toString()
+    filtering = QString( "XMCDA files (*.xmcda)" )
+    encode = settings.value( "/UI/encoding" ).toString()
+    fileDialog = QgsEncodingFileDialog( parent, "Save output XMCDA file", dirName, filtering, encode )
+    fileDialog.setDefaultSuffix( QString( "xmcda" ) )
+    fileDialog.setFileMode( QFileDialog.AnyFile )
+    fileDialog.setAcceptMode( QFileDialog.AcceptSave )
+    fileDialog.setConfirmOverwrite( True )
+    if not fileDialog.exec_() == QDialog.Accepted:
+            return None, None
+    files = fileDialog.selectedFiles()
+    settings.setValue("/UI/lastXMCDAfileDir", QVariant( QFileInfo( unicode( files.first() ) ).absolutePath() ) )
+    return ( unicode( files.first() ), unicode( fileDialog.encoding() ) )
+
+def xmcda_load_dialog(parent):
+    settings = QSettings() 
+    dirName = settings.value("/UI/lastXMCDAfileDir").toString()
+    filtering = QString( "XMCDA files (*.xmcda)" )
+    encode = settings.value( "/UI/encoding" ).toString()
+    fileDialog = QgsEncodingFileDialog( parent, "Load input XMCDA file", dirName, filtering, encode )
+    fileDialog.setDefaultSuffix( QString( "xmcda" ) )
+    fileDialog.setFileMode( QFileDialog.AnyFile )
+    fileDialog.setAcceptMode( QFileDialog.AcceptOpen )
+    fileDialog.setConfirmOverwrite( False )
+    if not fileDialog.exec_() == QDialog.Accepted:
+            return None, None
+    files = fileDialog.selectedFiles()
+    settings.setValue("/UI/lastXMCDAfileDir", QVariant( QFileInfo( unicode( files.first() ) ).absolutePath() ) )
+    return ( unicode( files.first() ), unicode( fileDialog.encoding() ) )

xmcda.py
@@ -54,6 +54,55 @@ def format_performances_table(perfs_table):
     output += "</performanceTable>\n"
     return output
 
+def format_criteria_weights(weights):
+    output = "<criteriaValues>\n"
+    for crit, weight in weights.iteritems():
+        output += "\t<criterionValue>\n"
+        output += "\t\t<criterionID>%s</criterionID>\n" % crit
+        output += "\t\t<value><real>%s</real></value>\n" % weight
+        output += "\t</criterionValue>\n"
+    output += "</criteriaValues>\n"
+    return output
+
+def format_category_profiles(profiles, palts_id, cat_id):
+    output = "<categoriesProfiles>\n"
+    for i, profile in enumerate(profiles):
+        output += "\t<categoryProfile>\n"
+        output += "\t\t<alternativeID>%s</alternativeID>\n" % palts_id[i]
+        output += "\t\t<limits>\n"
+        output += "\t\t\t<lowerCategory><categoryID>%s</categoryID></lowerCategory>\n" % cat_id[i]
+        output += "\t\t\t<upperCategory><categoryID>%s</categoryID></upperCategory>\n" % cat_id[i+1]
+        output += "\t\t</limits>\n"
+        output += "\t</categoryProfile>\n"
+    output += "</categoriesProfiles>\n"
+    return output
+
+def format_pt_reference_alternatives(profiles, palts_id, crit_id):
+    output = "<performanceTable>\n"
+    output += "\t<description>\n"
+    output += "\t\t<title>Performance table of reference alternatives</title>\n"
+    output += "\t</description>\n"
+    for i, profile in enumerate(profiles):
+        output += "\t<alternativePerformances>\n"
+        output += "\t\t<alternativeID>%s</alternativeID>\n" % palts_id[i]
+        for j, crit in enumerate(crit_id): 
+           output += "\t\t<performance>\n"
+           output += "\t\t\t<criterionID>%s</criterionID>\n" % crit
+           output += "\t\t\t<value><real>%s</real></value>\n" % profile['refs'][crit]
+           output += "\t\t</performance>\n"
+        output += "\t</alternativePerformances>\n"
+
+    output += "</performanceTable>\n"
+    return output
+
+def format_lambda(lbda):
+    output = "<methodParameters>\n"
+    output += "\t<parameter name=\"lambda\">\n"
+    output += "\t\t<value><real>%s</real></value>\n" % lbda
+    output += "\t</parameter>\n"
+    output += "</methodParameters>\n"
+    return output
+
 def add_xmcda_tags(xml_data):
     output = '<?xml version="1.0" encoding="UTF-8"?>\n'
     output += '<?xml-stylesheet type="text/xsl" href="xmcdaXSL.xsl"?>\n'
@@ -113,3 +162,8 @@ def get_method_errors(xmltree):
     for xmlmsg in xmltree.findall(".//methodMessages/errorMessage/text"):
         errors.append(xmlmsg.text)
     return errors
+
+def save_file(filename, xmcda_data):
+    file = open(filename, "w")
+    file.write(add_xmcda_tags(xmcda_data))
+    file.close()

commit 3258064d10960c10f311b84b3bcda9a85895726f

[XMCDA] Fix bug introduced by commit b79030c6850d224afc168b459c93eee0857afee7

etrimain.py
@@ -635,6 +635,9 @@ class EtriMainWindow(QtGui.QMainWindow, Ui_EtriMainWindow):
         lbda = self.inference_out[3]
         compat_alts = self.inference_out[4]
 
+        for crit, val in weights.iteritems():
+            weights[crit] = val*100
+
         inference_dialog = InferenceDialog(self, self.on_inference_accept)
         inference_dialog.show()
         inference_dialog.set_xmcda_text("Inference Procedure results")
@@ -651,8 +654,7 @@ class EtriMainWindow(QtGui.QMainWindow, Ui_EtriMainWindow):
         criterion_names = {}
         for i in self.criteria_activated:
             criterion_names["%d" % i] = self.criteria[i]['name'] 
-        
-        weights = [(weight*100) for weight in weights]
+
         inference_dialog.set_weights(criterion_names, weights)
         inference_dialog.set_profiles(criterion_names, profiles)
 