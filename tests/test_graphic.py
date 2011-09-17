66dfb56d181560f38afe6ad41fbe2bb85cd78693
Olivier Sobrie <olivier@sobrie.be>
2011-09-17 15:59:27 +0200

    [TESTS] Update graphic tests so that the resizing is possible

--- tests/test_graphic.py
+++ tests/test_graphic.py
@@ -7,12 +7,21 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from data_ticino import *
 
+class mygraphicsview(QtGui.QGraphicsView):
+
+    def __init__(self, parent = None):
+        super(QtGui.QGraphicsView, self).__init__(parent)
+
+    def resizeEvent(self, event):
+        scene = self.scene()
+        scene.update(self.size())
+        self.resetCachedContent()
+
 if __name__ == "__main__":
     etri = electre_tri(a, profiles, w, lbda, d)
 
     app = QtGui.QApplication(sys.argv)
-    view = QtGui.QGraphicsView()
-    #view.setAlignment(QtCore.Qt.AlignBottom)
+    view = mygraphicsview()
     view.setRenderHint(QtGui.QPainter.Antialiasing)
     layout = QtGui.QVBoxLayout()
     layout.addWidget(view)
--- tests/test_graphic2.py
+++ tests/test_graphic2.py
@@ -8,11 +8,21 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from data_loulouka import *
 
+class mygraphicsview(QtGui.QGraphicsView):
+
+    def __init__(self, parent = None):
+        super(QtGui.QGraphicsView, self).__init__(parent)
+
+    def resizeEvent(self, event):
+        scene = self.scene()
+        scene.update(self.size())
+        self.resetCachedContent()
+
 if __name__ == "__main__":
     etri = electre_tri(a, profiles, w, lbda)
 
     app = QtGui.QApplication(sys.argv)
-    view = QtGui.QGraphicsView()
+    view = mygraphicsview()
     view.setRenderHint(QtGui.QPainter.Antialiasing)
     layout = QtGui.QVBoxLayout()
     layout.addWidget(view)
