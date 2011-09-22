8b4783bdbd9ebd45541c6f1c4855e3927be405cf
Olivier Sobrie <olivier@sobrie.be>
2011-09-22 19:06:17 +0200

    [MCDA] Update MCDA class and add test data defined with this class

--- mcda/types.py
+++ mcda/types.py
@@ -6,6 +6,8 @@ class criterion:
     def __init__(self, id, name=None, disabled=None, direction=None,
                  weight=None):
         self.id = id
+        if name == None:
+            self.name = str(id)
         self.name = name
         if disabled == None:
             disabled = 0
@@ -18,21 +20,29 @@ class criterion:
         self.weight = weight
 
     def __repr__(self):
-        return "[%s]%s: %g" % (self.id, self.name,
-                               int(self.direction)*float(self.weight))
+        return "C_%s: %g" % (self.name,
+                             int(self.direction)*float(self.weight))
 
 class action:
 
     def __init__(self, id=None, name=None, evaluations=None):
         self.id = id
+        if name == None:
+            self.name = str(id)
         self.name = name
         self.evaluations = evaluations
 
     def __repr__(self):
-        return "[%s]%s" % (self.id, self.name)
-
-class profile(action):
-    pass
+        return "A_%s: %s" % (self.name, self.evaluations)
 
 class threshold(action):
     pass
+
+class profile(action):
+
+    def __init__(self, id=None, name=None, evaluations=None,
+                 indifference=None, preference=None, veto=None):
+        action.__init__(self, id, name, evaluations)
+        self.indifference = indifference
+        self.preference = preference
+        self.veto = veto
---
+++ tests/data_ticino_new.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import sys
+sys.path.insert(0, "..")
+from mcda.types import criterion, action, profile, threshold
+
+# Criteria
+prix = criterion('prix', 'prix', 0, -1, 25)
+transport = criterion('transport', 'transport', 0, -1, 45)
+envir = criterion('envir', 'environment', 0, 1, 10)
+residents = criterion('residents', 'residents', 0, 1, 12)
+competition = criterion('competition', 'competition', 0, 1, 8)
+criteria = [ prix, transport, envir, residents, competition ]
+
+# Actions
+a1 = action('a1', 'a1', {prix: 120, transport:  284, envir: 5, residents: 3.5, competition: 18})
+a2 = action('a2', 'a2', {prix: 150, transport:  269, envir: 2, residents: 4.5, competition: 24})
+a3 = action('a3', 'a3', {prix: 100, transport:  413, envir: 4, residents: 5.5, competition: 17})
+a4 = action('a4', 'a4', {prix:  60, transport:  596, envir: 6, residents: 8.0, competition: 20})
+a5 = action('a5', 'a5', {prix:  30, transport: 1321, envir: 8, residents: 7.5, competition: 16})
+a6 = action('a6', 'a6', {prix:  80, transport:  734, envir: 5, residents: 4.0, competition: 21})
+a7 = action('a7', 'a7', {prix:  45, transport:  982, envir: 7, residents: 8.5, competition: 13})
+a = [ a1, a2, a3, a4, a5, a6, a7 ]
+
+# Reference actions
+b1 = {prix: 100, transport: 1000, envir: 4, residents: 4, competition: 15}
+b2 = {prix:  50, transport:  500, envir: 7, residents: 7, competition: 20}
+
+# Indifference, Preference and Veto
+q = threshold('q', 'indifference',  {'prix': 15,  'transport':  80, 'envir': 1, 'residents': 0.5, 'competition': 1})
+p = threshold('p', 'preference', {'prix': 40,  'transport': 350, 'envir': 3, 'residents': 3.5, 'competition': 5})
+v = threshold('v', 'veto', {'prix': 100, 'transport': 850, 'envir': 5, 'residents': 4.5, 'competition': 8})
+
+# Profiles
+prof1 = profile('p1', 'profile_down', b1, q, p, v)
+prof2 = profile('p2', 'profile_up', b2, q, p, v)
+profiles = [ prof1, prof2 ]
+
+# Affecations
+affect_p = {a1: 2, a2: 1, a3: 2, a4: 3, a5: 1, a6: 2, a7:2}
+affect_o = {a1: 2, a2: 3, a3: 2, a4: 3, a5: 2, a6: 2, a7:2}
+
+# Lambda
+lbda = 0.75
