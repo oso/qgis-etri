ba6dd9fd87062c8536eb561de2559882f16523c5
Olivier Sobrie <olivier@sobrie.be>
2011-10-30 20:22:17 +0100

    [TABLE] Add button to save the values into xmcda format

--- tests/test_table.py
+++ tests/test_table.py
@@ -5,12 +5,28 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from mcda.types import criterion
 from table import criteria_table, qt_performance_table, qt_threshold_table
+from xml.etree import ElementTree
 
 crit_table = None
 prof_table = None
 indif_table = None
 pref_table = None
 veto_table = None
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
 
 def criterion_direction_changed(criterion):
     print criterion.id, ":", criterion.direction
@@ -26,6 +42,32 @@ def add_tab(tabs, table, name):
     layout = QtGui.QVBoxLayout(tab)
     layout.addWidget(table)
     tabs.addTab(tab, name)
+
+def save_dialog_box():
+    fname = QtGui.QFileDialog.getSaveFileName(None,
+                                              "Save XMCDA file", ".",
+                                              "XMCDA files (*.xmcda)")
+    fname = unicode(fname)
+    if fname:
+        if "." not in fname:
+            fname += ".xmcda"
+
+    return fname
+
+def save_to_xmcda():
+    fname = save_dialog_box()
+    if fname:
+        root = ElementTree.Element('{http://www.decision-deck.org/2009/XMCDA-2.1.0}XMCDA')
+        pt_xmcda = ptb.to_xmcda()
+        criteria_xmcda = c.to_xmcda()
+        alternatives_xmcda = b.to_xmcda()[0]
+        root.append(pt_xmcda)
+        root.append(criteria_xmcda)
+        root.append(alternatives_xmcda)
+        ElementTree.dump(root)
+        indent(root)
+        ElementTree.ElementTree(root).write(fname, xml_declaration=True,
+                                            encoding='utf-8', method='xml')
 
 if __name__ == "__main__":
 
@@ -52,12 +94,17 @@ if __name__ == "__main__":
     add_tab(tabs, pref_table, "Preference")
     add_tab(tabs, veto_table, "Veto")
 
+    button_to_xmcda = QtGui.QPushButton("Save to XMCDA")
+
     crit_table.connect(crit_table,
                        QtCore.SIGNAL("criterion_state_changed"),
                        criterion_state_changed)
     crit_table.connect(crit_table,
                        QtCore.SIGNAL("criterion_direction_changed"),
                        criterion_direction_changed)
+    button_to_xmcda.connect(button_to_xmcda,
+                            QtCore.SIGNAL("clicked()"),
+                            save_to_xmcda)
 
     print "# of criteria:", crit_table.ncriteria
     print "Criteria enabled:", crit_table.criteria_enabled
@@ -65,6 +112,7 @@ if __name__ == "__main__":
     layout = QtGui.QVBoxLayout()
     layout.addWidget(crit_table)
     layout.addWidget(tabs)
+    layout.addWidget(button_to_xmcda)
     dialog = QtGui.QDialog()
     dialog.setLayout(layout)
     dialog.resize(640, 480)
