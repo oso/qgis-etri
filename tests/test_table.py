583256237cc7982ca5fc821b34ee9414d2db877d
Olivier Sobrie <olivier@sobrie.be>
2011-11-12 16:53:12 +0100

    [COLORS] Put display category colors in colors.py

--- tests/test_table.py
+++ tests/test_table.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.insert(0, "..")
 import copy
+import colors
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 from mcda.types import criterion
@@ -15,92 +16,6 @@ indif_table = None
 pref_table = None
 veto_table = None
 thresh_cbox = None
-
-categories_colors_2 = {1: QtGui.QColor(252, 141, 89),
-                       2: QtGui.QColor(145, 207, 96)}
-
-categories_colors_3 = {1: QtGui.QColor(252, 141, 89),
-                       2: QtGui.QColor(255, 255, 191),
-                       3: QtGui.QColor(145, 207, 96)}
-
-categories_colors_4 = {1: QtGui.QColor(215, 25, 28),
-                       2: QtGui.QColor(253, 174, 97),
-                       3: QtGui.QColor(166, 217, 106),
-                       4: QtGui.QColor(26, 150, 65)}
-
-categories_colors_5 = {1: QtGui.QColor(215, 48, 39),
-                       2: QtGui.QColor(253, 174, 97),
-                       3: QtGui.QColor(255, 255, 191),
-                       4: QtGui.QColor(166, 217, 106),
-                       5: QtGui.QColor(26, 150, 65)}
-
-categories_colors_6 = {1: QtGui.QColor(215, 48, 39),
-                       2: QtGui.QColor(252, 141, 89),
-                       3: QtGui.QColor(254, 224, 139),
-                       4: QtGui.QColor(217, 239, 139),
-                       5: QtGui.QColor(145, 207, 96),
-                       6: QtGui.QColor(26, 152, 80)}
-
-categories_colors_7 = {1: QtGui.QColor(215, 48, 39),
-                       2: QtGui.QColor(252, 141, 89),
-                       3: QtGui.QColor(254, 224, 139),
-                       4: QtGui.QColor(255, 255, 191),
-                       5: QtGui.QColor(217, 239, 139),
-                       6: QtGui.QColor(145, 207, 96),
-                       7: QtGui.QColor(26, 152, 80)}
-
-categories_colors_8 = {1: QtGui.QColor(215, 48, 39),
-                       2: QtGui.QColor(244, 109, 67),
-                       3: QtGui.QColor(253, 174, 97),
-                       4: QtGui.QColor(254, 224, 139),
-                       5: QtGui.QColor(217, 239, 139),
-                       6: QtGui.QColor(166, 217, 106),
-                       7: QtGui.QColor(102, 189, 99),
-                       8: QtGui.QColor(26, 152, 80)}
-
-categories_colors_9 = {1: QtGui.QColor(215, 48, 39),
-                       2: QtGui.QColor(244, 109, 67),
-                       3: QtGui.QColor(253, 174, 97),
-                       4: QtGui.QColor(254, 224, 139),
-                       5: QtGui.QColor(255, 255, 191),
-                       6: QtGui.QColor(217, 239, 139),
-                       7: QtGui.QColor(166, 217, 106),
-                       8: QtGui.QColor(102, 189, 99),
-                       9: QtGui.QColor(26, 152, 80)}
-
-categories_colors_10 = {1: QtGui.QColor(165, 0, 38),
-                        2: QtGui.QColor(215, 48, 39),
-                        3: QtGui.QColor(244, 109, 67),
-                        4: QtGui.QColor(253, 174, 97),
-                        5: QtGui.QColor(254, 224, 139),
-                        6: QtGui.QColor(217, 239, 139),
-                        7: QtGui.QColor(166, 217, 106),
-                        8: QtGui.QColor(102, 189, 99),
-                        9: QtGui.QColor(26, 152, 80),
-                        10: QtGui.QColor(0, 104, 55)}
-
-categories_colors_11 = {1: QtGui.QColor(165, 0, 38),
-                        2: QtGui.QColor(215, 48, 39),
-                        3: QtGui.QColor(244, 109, 67),
-                        4: QtGui.QColor(253, 174, 97),
-                        5: QtGui.QColor(254, 224, 139),
-                        6: QtGui.QColor(255, 255, 191),
-                        7: QtGui.QColor(217, 239, 139),
-                        8: QtGui.QColor(166, 217, 106),
-                        9: QtGui.QColor(102, 189, 99),
-                        10: QtGui.QColor(26, 152, 80),
-                        11: QtGui.QColor(0, 104, 55)}
-
-ncategories_colors = {2: categories_colors_2,
-                      3: categories_colors_3,
-                      4: categories_colors_4,
-                      5: categories_colors_5,
-                      6: categories_colors_6,
-                      7: categories_colors_7,
-                      8: categories_colors_8,
-                      9: categories_colors_9,
-                      10: categories_colors_10,
-                      11: categories_colors_11}
 
 def indent(elem, level=0):
     i = "\n" + level*"  "
@@ -241,8 +156,8 @@ def save_to_xmcda():
 def run_electre_tri():
     etri = electre_tri(c, pt, ptb, lbda)
     affectations = etri.pessimist()
-    colors = ncategories_colors[len(b)+1]
-    perf_table.add_affectations(affectations, colors)
+    cat_colors = colors.ncategories_colors[len(b)+1]
+    perf_table.add_affectations(affectations, cat_colors)
 
 if __name__ == "__main__":
 
