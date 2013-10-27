b7932f9ac9bb052ff0cb450d7a1f3ccd7fc11786
Olivier Sobrie <olivier@sobrie.be>
2013-10-27 20:08:23 +0100

    __init__: remove useless qgis 1.x methods

--- __init__.py
+++ __init__.py
@@ -1,19 +1,4 @@
 from etri_plugin import etri_plugin
 
-def name():
-    return "Electre Tri plugin"
-
-def description():
-    return "A decision aid plugin based on ELECTRE TRI"
-
-def version():
-    return "0.2.1"
-
-def qgisMinimumVersion():
-    return "1.4"
-
-def authorName():
-    return "Olivier Sobrie"
-
 def classFactory(iface):
     return etri_plugin(iface)
